c70ef070f65e96106589acf6753dad7a91e153c1
fedi <fadikossentini@insat.u-carthage.tn>
2022-03-24 15:11:02 +0100

    meladetails

--- melas/lib/main.dart
+++ melas/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:melas/screens/categories_screen.dart';
 import 'package:melas/screens/category_meals_screen.dart';
+import 'package:melas/screens/meal_detail_screeb.dart';
 
 void main() => runApp(MyApp());
 
@@ -43,6 +44,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
     );
   }
